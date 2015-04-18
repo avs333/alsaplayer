avs333/alsaplayer

commit 63f9be1c61bb87ee14d6f258df51c23f8dced9a8

fixed offload

app/jni/alsa.c
@@ -98,6 +98,7 @@ void alsa_exit(playback_ctx *ctx)
 	if(priv->nv_start) free_nvset(priv->nv_start);
 	if(priv->nv_stop) free_nvset(priv->nv_stop);
 	if(priv->xml_dev) xml_dev_close(priv->xml_dev);
+	if(priv->devinfo) free(priv->devinfo);
 	for(k = 0; k < n_supp_rates; k++) 
 	    if(priv->nv_rate[k]) free(priv->nv_rate[k]);
 	for(k = 0; k < n_supp_formats; k++) { 
@@ -172,6 +173,29 @@ static inline void setup_hwparams(struct snd_pcm_hw_params *params,
     if(periods) param_set_int(params, SNDRV_PCM_HW_PARAM_PERIODS, periods);
 }
 
+static inline char *cat_str(char *dest, const char *src)
+{
+    char *ret;
+	if(dest) {
+	    ret = (char *) realloc(dest, strlen(dest)+strlen(src)+1);
+	    return strcat(ret, src);	
+	} else {
+	    ret = (char *) malloc(strlen(src)+1);
+	    return strcpy(ret, src);	
+	}
+}
+
+static const char *compr_codecs[SND_AUDIOCODEC_MAX+1] = {
+   [0x1] = "SND_AUDIOCODEC_PCM", [0x2] = "SND_AUDIOCODEC_MP3", [0x3] = "SND_AUDIOCODEC_AMR",	
+   [0x4] = "SND_AUDIOCODEC_AMRWB", [0x5] = "SND_AUDIOCODEC_AMRWBPLUS", [0x6] = "SND_AUDIOCODEC_AAC",
+   [0x7] = "SND_AUDIOCODEC_WMA", [0x8] = "SND_AUDIOCODEC_REAL", [0x9] = "SND_AUDIOCODEC_VORBIS",
+   [0xA] = "SND_AUDIOCODEC_FLAC", [0xB] = "SND_AUDIOCODEC_IEC61937", [0xC] = "SND_AUDIOCODEC_G723_1",
+   [0xD] = "SND_AUDIOCODEC_G729", [0xE] = "SND_AUDIOCODEC_AC3", [0xF] = "SND_AUDIOCODEC_DTS",
+   [0x10] = "SND_AUDIOCODEC_AC3_PASS_THROUGH", [0x11] = "SND_AUDIOCODEC_WMA_PRO", [0x12] = "SND_AUDIOCODEC_DTS_PASS_THROUGH",
+   [0x13] = "SND_AUDIOCODEC_DTS_LBR", [0x14] = "SND_AUDIOCODEC_DTS_TRANSCODE_LOOPBACK", [0x15] = "SND_AUDIOCODEC_PASS_THROUGH",
+   [0x16] = "SND_AUDIOCODEC_MP2", [0x17] = "SND_AUDIOCODEC_DTS_LBR_PASS_THROUGH", [0x18] = "SND_AUDIOCODEC_EAC3",
+};
+
 /* Switch to card/device if it's changed: 
    Initialise device controls, parse xml file (if any) to find device description 
    and controls required to handle it, and determine hw support features.  */
@@ -185,6 +209,7 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
     void *xml_dev = 0;
     struct snd_pcm_hw_params hwparams;
     struct snd_compr_caps caps;
+    char *c = 0;
 
 	if(!ctx) {
 	    log_err("no context");
@@ -307,16 +332,23 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 	    goto err_exit;	
 	}
 
+	c = cat_str(c, tmp);
+	c = cat_str(c, priv->is_offload ? " (offload)\n" : " (pcm)\n");
+
 	if(priv->is_offload) {
 	    if(ioctl(fd, SNDRV_COMPRESS_GET_CAPS, &caps) != 0) {
 		log_err("cannot get compress capabilities for device %d", device);
 		ret = LIBLOSSLESS_ERR_AU_SETUP;
 		goto err_exit;
 	    }
+    	    c = cat_str(c, "Supported codecs:\n");
 	    for(k = 0; k < caps.num_codecs; k++) {
-		if(caps.codecs[k] >= 32) continue;
-		priv->supp_codecs_mask |= (1 << caps.codecs[k]);
-	    }	
+		int i = caps.codecs[k];
+		if(i <= 0 || i >= SND_AUDIOCODEC_MAX) continue;
+		c = cat_str(c, compr_codecs[i]);
+		c = cat_str(c, "\n");
+		priv->supp_codecs_mask |= (1 << i);
+	    }
 #if 0
 	    if(priv->supp_codecs_mask == 0) { 
 		log_err("this device cannot play any sane compressed streams");
@@ -331,11 +363,13 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 	    }
 #endif	
 	}
-
+    	c = cat_str(c, "Supported formats:\n");
 	for(k = 0; k < n_supp_formats; k++) {	
 	    if(!priv->is_offload) setup_hwparams(&hwparams, supp_formats[k].fmt, 0, 0, 0, 0);
 	    if(priv->is_offload || ioctl(fd, SNDRV_PCM_IOCTL_HW_REFINE, &hwparams) == 0) {
 		priv->supp_formats_mask |= supp_formats[k].mask;
+		c = cat_str(c, supp_formats[k].str);
+		c = cat_str(c, "\n");
 		if(!xml_dev) continue;
 		priv->nv_fmt[k] = xml_dev_find_ctls(xml_dev, "fmt", supp_formats[k].str);
 		if(priv->nv_fmt[k]) log_info("found controls for fmt=%s", supp_formats[k].str);
@@ -356,13 +390,15 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 		}
 	    }
 	}
+	c = cat_str(c, "Supported samplerates:\n");
 	for(k = 0; k < n_supp_rates; k++) {
 	    int rate = supp_rates[k].rate;	
 	    if(!priv->is_offload) setup_hwparams(&hwparams, 0, rate, 0, 0, 0);	
 	    if(priv->is_offload || ioctl(fd, SNDRV_PCM_IOCTL_HW_REFINE, &hwparams) == 0) {
 		priv->supp_rates_mask |= supp_rates[k].mask;
+		sprintf(tmp, "%d ", rate);
+		c = cat_str(c, tmp);
 		if(xml_dev) {
-		    sprintf(tmp, "%d", rate);
 		    priv->nv_rate[k] = xml_dev_find_ctls(xml_dev, "rate", tmp);
 		    if(priv->nv_rate[k]) log_info("found controls for rate=%d", rate);
 		    else log_info("will use defaults for rate=%d", rate);
@@ -375,6 +411,7 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 	    ret = LIBLOSSLESS_ERR_AU_GETCONF;
 	    goto err_exit;	
 	}
+	priv->devinfo = c;
 	close(fd);
 	priv->xml_dev = xml_dev;	
 	if(priv->is_offload) log_info("selected card %d device %d for offload playback", card, device);	
@@ -385,6 +422,7 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
     err_exit:
 	if(fd >= 0) close(fd);
 	if(xml_dev) xml_dev_close(xml_dev);
+	if(c) free(c);
 	alsa_exit(ctx);
     return ret;	
 
@@ -1041,21 +1079,21 @@ static int get_card_info(int card, struct snd_ctl_card_info *info)
 	fd = open(tmp, O_RDONLY);
 	if(fd < 0) {
 	    log_err("failed to open card %d", card);
-	    return 0;
+	    return -1;
 	}
 	k = ioctl(fd, SNDRV_CTL_IOCTL_CARD_INFO, info);
 	close(fd);
 	if(k != 0) { 
 	    log_err("failed to get info for card %d", card);
-	    return 0;
+	    return -1;
 	}
-    return 1;
+    return 0;
 }
 
 int alsa_is_usb_card(JNIEnv *env, jobject obj, int card) 
 {
     struct snd_ctl_card_info info;
-    if(!get_card_info(card, &info)) return 0;
+    if(get_card_info(card, &info) != 0) return 0;
     return strcmp((char *)info.driver, "USB-Audio") == 0;
 }
 
@@ -1064,7 +1102,7 @@ int alsa_is_offload_device(JNIEnv *env, jobject obj, int card, int device)
     void *xml = 0;
     struct snd_ctl_card_info info;
     int ret;
-	if(!get_card_info(card, &info)) return 0;
+	if(get_card_info(card, &info) != 0) return 0;
 	xml = xml_dev_open(cards_file, (char *) info.name, device);
 	if(!xml) return 0;
 	ret = xml_dev_is_offload(xml);
@@ -1090,6 +1128,7 @@ int alsa_get_devices(char ***dev_names)
 	    sprintf(tmp, "/dev/snd/controlC%d", k);
 	    fd = open(tmp, O_RDONLY);
 	    if(fd < 0) break;
+	    memset(&info, 0, sizeof(info));	
 	    if(ioctl(fd, SNDRV_CTL_IOCTL_CARD_INFO, &info) != 0) break;
 	    xml = xml_dev_open(cards_file, (char *) info.name, -1);
 	    i = -1;
@@ -1115,6 +1154,12 @@ int alsa_get_devices(char ***dev_names)
 	if(n) *dev_names = devs;
 
     return n;	
+}
+
+char *alsa_current_device_info(playback_ctx *ctx)
+{
+    if(!ctx || !ctx->alsa_priv) return 0;	
+    return ((alsa_priv *) ctx->alsa_priv)->devinfo;		
 }
 #endif
 

app/jni/main.c
@@ -552,6 +552,12 @@ static jobjectArray get_devices(JNIEnv *env, jobject obj)
     return ja;
 }
 
+static jstring current_device_info(JNIEnv *env, jobject obj, playback_ctx *ctx)
+{
+    char *c = alsa_current_device_info(ctx);
+    return c ? (*env)->NewStringUTF(env,c) : 0;
+}
+
 static jboolean libinit(JNIEnv *env, jobject obj, jint sdk) 
 {
     return true;
@@ -613,6 +619,7 @@ static JNINativeMethod methods[] = {
  { "audioPlay", "(ILjava/lang/String;II)I", (void *) audio_play },
  { "extractFlacCUE", "(Ljava/lang/String;)[I", (void *) extract_flac_cue },
  { "getAlsaDevices", "()[Ljava/lang/String;", (void *) get_devices },
+ { "getCurrentDeviceInfo", "(I)Ljava/lang/String;", (void *) current_device_info },
  { "isUsbCard", "(I)Z", (void *) alsa_is_usb_card },
  { "inOffloadMode", "(I)Z", (void *) in_offload_mode },
  { "isOffloadDevice", "(II)Z", (void *) alsa_is_offload_device },

app/jni/main.h
@@ -115,6 +115,7 @@ extern int alsa_is_offload(playback_ctx *ctx);
 extern int alsa_get_devices(char ***dev_names);
 extern int alsa_is_usb_card(JNIEnv *env, jobject obj, int card);
 extern int alsa_is_offload_device(JNIEnv *env, jobject obj, int card, int device);
+extern char *alsa_current_device_info(playback_ctx *ctx);
 #endif
 
 /* alsa_offload.c */

app/src/main/res/values-ru/strings.xml
@@ -70,7 +70,7 @@
 <string name="strMplayerError">Ошибка медиа-плеера</string>
 <string name="strDriverMode">Прямой доступ к драйверу</string>
 <string name="strSaveBooks">Автосохранение закладок</string>
-<string name="strAbout">Android плеер alsaPlayer для звуковых файлов без потерь. Дата сборки Jan 22, 2015</string>
+<string name="strAbout">Android плеер alsaPlayer для звуковых файлов без потерь. Дата сборки Apr  2, 2015</string>
 <string name="strAbout1">О программе</string>
 <string name="strQuit">Выход</string>
 <string name="strHideExt">Не показывать расширений файлов</string>

commit 0ad308eb13694cb776e119531b5eef6a6e626e6a

fix samplerate bug introduced in 63f9be1; cmd line asm + options; rough 64-bit build

.gitignore
@@ -13,4 +13,4 @@
 gradle.properties
 /app/jni/tinyxml/xmltest.c
 /libsuperuser/build
-
+/app/jni/alsaplayer

app/jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := lossless
 LOCAL_STATIC_LIBRARIES := flac ape xmlparser
-LOCAL_CFLAGS += -march=armv7-a -O3 -Wall -finline-functions -fPIC -Iinclude 
+LOCAL_CFLAGS += -O3 -Wall -finline-functions -fPIC -Iinclude 
 LOCAL_CFLAGS += -DHAVE_CONFIG_H -DCLASS_NAME=\"net/avs234/alsaplayer/AlsaPlayerSrv\"
 LOCAL_CFLAGS += -DBUILD_STANDALONE -DCPU_ARM
 #LOCAL_ARM_MODE := arm

app/jni/Application.mk
@@ -1,1 +1,3 @@
 APP_PLATFORM := android-16
+APP_ABI := armeabi-v7a arm64-v8a
+

app/jni/Makefile
@@ -2,6 +2,7 @@
 # use "make android=1" to build executable for android
  
 ifdef android
+# Tested with Android NDK r10d toolchain. Tweak this if you have other compiler/NDK.
 GCCVER=4.9
 ANDPLAT=android-18
 HOSTARCH=$(shell uname -m)
@@ -25,6 +26,11 @@ endif
 
 SRC =	flac/shndec.c  flac/main.c  flac/decoder.c  buffer.c alsa.c alsa_offload.c main.c linux_main.c wav_main.c	\
 	ape/entropy.c  ape/filter-pre.c  ape/parser.c   ape/decoder.c  ape/main.c  ape/predictor.c	
+
+ifdef android
+CFLAGS += -DCPU_ARM -DARM_ARCH=7 -mfpu=neon -mfloat-abi=softfp
+SRC += flac/arm.S ape/predictor-arm.S
+endif
 
 SRCX =	tinyxml/tinyxml2.cpp  tinyxml/xmlparser.cpp
 

app/jni/alsa.c
@@ -66,10 +66,12 @@ static const struct _supp_rates {
 
 static int init_mixer_controls(playback_ctx *ctx, int card);
 
-#if defined(ANDROID) || defined(ANDLINUX)
+#if defined(ANDROID) 
 static char cards_file[] = "/sdcard/.alsaplayer/cards.xml";
 #else
 static char cards_file[PATH_MAX];
+char *ext_cards_file = 0;
+int forced_chunks = 0, forced_chunk_size = 0;
 #endif
 
 /* free per track params */
@@ -259,8 +261,18 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 	priv->card_name = strdup((char *)info.name);
 	close(fd); 
 	fd = -1;
-#if !defined(ANDROID) && !defined(ANDLINUX)
-	sprintf(cards_file, "%s/.alsaplayer/cards.xml", getenv("HOME"));	
+#if !defined(ANDROID)
+	if(ext_cards_file) {
+	    strcpy(cards_file, ext_cards_file);
+	    log_info("using custom config from %s", ext_cards_file);
+	} else {
+#if defined(ANDLINUX)
+	    strcpy(cards_file,"/sdcard/.alsaplayer/cards.xml");
+#else
+	    sprintf(cards_file, "%s/.alsaplayer/cards.xml", getenv("HOME"));	
+#endif
+	    log_info("using default config from %s", cards_file);
+	}
 #endif
 	xml_dev = xml_dev_open(cards_file, priv->card_name, device);	
 	if(!xml_dev) log_info("warning: no settings for card %d (%s) device %d in %s", card, priv->card_name, device, cards_file); 
@@ -342,9 +354,9 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 		goto err_exit;
 	    }
     	    c = cat_str(c, "Supported codecs:\n");
-	    for(k = 0; k < caps.num_codecs; k++) {
+	    for(k = 0; k < MAX_NUM_CODECS /* caps.num_codecs */; k++) { /* for msm kernel bug */
 		int i = caps.codecs[k];
-		if(i <= 0 || i >= SND_AUDIOCODEC_MAX) continue;
+		if(i <= 0 || i > SND_AUDIOCODEC_MAX) continue; 
 		c = cat_str(c, compr_codecs[i]);
 		c = cat_str(c, "\n");
 		priv->supp_codecs_mask |= (1 << i);
@@ -396,8 +408,9 @@ int alsa_select_device(playback_ctx *ctx, int card, int device)
 	    if(!priv->is_offload) setup_hwparams(&hwparams, 0, rate, 0, 0, 0);	
 	    if(priv->is_offload || ioctl(fd, SNDRV_PCM_IOCTL_HW_REFINE, &hwparams) == 0) {
 		priv->supp_rates_mask |= supp_rates[k].mask;
-		sprintf(tmp, "%d ", rate);
+		sprintf(tmp, "%d", rate);
 		c = cat_str(c, tmp);
+		c = cat_str(c, " ");
 		if(xml_dev) {
 		    priv->nv_rate[k] = xml_dev_find_ctls(xml_dev, "rate", tmp);
 		    if(priv->nv_rate[k]) log_info("found controls for rate=%d", rate);
@@ -516,8 +529,14 @@ int alsa_start(playback_ctx *ctx)
 	log_info("Period size: min=%d\tmax=%d", persz_min, persz_max);
 	log_info("    Periods: min=%d\tmax=%d", periods_min, periods_max);
 
+#ifndef ANDROID	
+	priv->chunks = forced_chunks ? forced_chunks : periods_max;
+	priv->chunk_size = forced_chunk_size ? forced_chunk_size : persz_max;
+#else
 	priv->chunks = periods_max;
 	priv->chunk_size = persz_max;
+	if(priv->chunks > 32) priv->chunks = 32; 
+#endif
 
 	param_set_int(params, SNDRV_PCM_HW_PARAM_PERIOD_SIZE, priv->chunk_size);
 	param_set_int(params, SNDRV_PCM_HW_PARAM_PERIODS, priv->chunks);
@@ -525,13 +544,19 @@ int alsa_start(playback_ctx *ctx)
 	/* Try to obtain the largest buffer possible keeping in mind 
 	   that ALSA always tries to set minimum latency */
 
-	if(priv->chunks > 32) priv->chunks = 32; 
-
 #define NSTEPS	8
 
 	i = (persz_max - persz_min) / NSTEPS; 
 
 	while(ioctl(priv->fd, SNDRV_PCM_IOCTL_HW_PARAMS, params) < 0) {
+#ifndef ANDROID
+	    if(forced_chunks || forced_chunk_size) {
+		log_err("cannot set forced hw parameters num=%d size=%d", 
+			forced_chunks, forced_chunk_size);
+		ret = LIBLOSSLESS_ERR_AU_SETCONF;
+		goto err_exit;
+	    }
+#endif		
 	    priv->chunk_size -= i;
 	    if(priv->chunk_size < persz_min || i == 0) {
 		priv->chunk_size = persz_max;
@@ -553,6 +578,11 @@ int alsa_start(playback_ctx *ctx)
 	}
 	priv->chunk_size = param_to_interval(params, SNDRV_PCM_HW_PARAM_PERIOD_SIZE)->max;
 
+#ifndef ANDROID
+	if(forced_chunks)
+	    log_info("forcing period size %d, periods %d", priv->chunk_size, priv->chunks);
+	else
+#endif
 	log_info("selecting period size %d, periods %d", priv->chunk_size, priv->chunks);
 
 	priv->buf_bytes = priv->chunk_size * ctx->channels * priv->format->phys_bits/8;
@@ -640,8 +670,8 @@ filled with silence. Note: silence_threshold must be set to zero.  */
 
     return ret;
 }
-/* Count in samples. */
-ssize_t alsa_write(playback_ctx *ctx, size_t count)
+/* Count in samples. If buf is zero, take data from priv->buf */
+ssize_t alsa_write(playback_ctx *ctx, void *buf, size_t count)
 {
     alsa_priv *priv;
     int i, written = 0;
@@ -653,19 +683,24 @@ ssize_t alsa_write(playback_ctx *ctx, size_t count)
 	    return 0;
 	}
 	priv = (alsa_priv *) ctx->alsa_priv;	
+
+	xf.buf = buf ? buf : priv->buf;
+	xf.frames = priv->chunk_size;
+	xf.result = 0;
 	
 	if(count > priv->chunk_size) {
 	    log_err("frames count %d larger than period size %d", (int) count, priv->chunk_size);
 	    count = priv->chunk_size;
 	} else if(count < priv->chunk_size) {
 	    log_info("short buffer, must be EOF");	
+	    if(buf) {
+		memcpy(priv->buf, buf, count * ctx->channels * priv->format->phys_bits/8);
+	    	xf.buf = priv->buf;	
+	    }		
 	    memset(priv->buf + count * ctx->channels * priv->format->phys_bits/8, 0, 
 			(priv->chunk_size - count) * ctx->channels * priv->format->phys_bits/8);
 	}	
 
-	xf.buf = priv->buf;
-	xf.frames = priv->chunk_size;
-	xf.result = 0;
 
 	while(written < priv->chunk_size) {
 #if 1
@@ -1156,11 +1191,12 @@ int alsa_get_devices(char ***dev_names)
     return n;	
 }
 
+#endif
+
 char *alsa_current_device_info(playback_ctx *ctx)
 {
     if(!ctx || !ctx->alsa_priv) return 0;	
     return ((alsa_priv *) ctx->alsa_priv)->devinfo;		
 }
-#endif
 
 

app/jni/alsa_offload.c
@@ -160,10 +160,14 @@ int alsa_play_offload(playback_ctx *ctx, int fd, off_t start_offset)
 	    goto err_exit;
 	}
 
+	log_info("device reports: max %d chunks of min %d size", caps.max_fragments, caps.min_fragment_size);	
+#ifndef ANDROID
+	priv->chunks = forced_chunks ? forced_chunks : caps.max_fragments;
+	priv->chunk_size = forced_chunk_size ? forced_chunk_size : caps.min_fragment_size;
+#else
 	priv->chunks = caps.max_fragments;
 	priv->chunk_size = caps.min_fragment_size;
-	log_info("device reports: max %d chunks of min %d size", priv->chunks, priv->chunk_size);	
-
+#endif
 	k = alsa_get_rate(ctx->samplerate);
 	if(k < 0) {
 	    log_err("unsupported playback rate");
@@ -217,6 +221,14 @@ int alsa_play_offload(playback_ctx *ctx, int fd, off_t start_offset)
 		params.buffer.fragments, params.buffer.fragment_size); */
 
 	while(ioctl(priv->fd, SNDRV_COMPRESS_SET_PARAMS, &params) != 0) {
+#ifndef ANDROID
+	    if(forced_chunks || forced_chunk_size) {
+                log_err("failed to set forced parameters for fragments num=%d size=%d",
+                        forced_chunks, forced_chunk_size);
+		ret = LIBLOSSLESS_ERR_AU_SETUP;
+		goto err_exit;
+	    }	
+#endif
 	    priv->chunks >>= 1;
 	    if(!priv->chunks) {
 		log_err("failed to set hardware parameters");
@@ -268,6 +280,15 @@ int alsa_play_offload(playback_ctx *ctx, int fd, off_t start_offset)
 	    goto err_exit;	
 	}
 	mptr += priv->chunks * read_size;
+
+	if(write_buff) {	/* free extra memory */
+	    write_buff = (int8_t *) realloc(write_buff, priv->chunk_size);
+	    if(!write_buff) {
+		log_err("no memory");
+		ret = LIBLOSSLESS_ERR_NOMEM;
+		goto err_exit;
+	    }		
+	}
 
 	log_info("starting playback");
 	if(ioctl(priv->fd, SNDRV_COMPRESS_START) != 0) {

app/jni/ape/Android.mk
@@ -3,12 +3,17 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := ape
 
-LOCAL_SRC_FILES +=  predictor.c decoder.c predictor-arm.S entropy.c parser.c filter-pre.c main.c
+LOCAL_SRC_FILES +=  predictor.c decoder.c entropy.c parser.c filter-pre.c main.c
 #	filter_1280_15.c filter_16_11.c filter_256_13.c filter_32_10.c filter_64_11.c main.c
 
-LOCAL_CFLAGS += -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp -O3 -Wall -DBUILD_STANDALONE -DCPU_ARM \
- -fPIC -DARM_ARCH=7 -UDEBUG -DNDEBUG -fomit-frame-pointer -ffreestanding -Iinclude -I.
+LOCAL_CFLAGS += -O3 -Wall -DBUILD_STANDALONE -fPIC -UDEBUG -DNDEBUG -fomit-frame-pointer -ffreestanding -Iinclude -I.
 
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+LOCAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp -DCPU_ARM -DARM_ARCH=7
+LOCAL_SRC_FILES += predictor-arm.S
+endif
+
+# -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 #LOCAL_CFLAGS += -O3 -Wall -DBUILD_STANDALONE -DCPU_ARM \
 # -fPIC -DARM_ARCH=5 -UDEBUG -DNDEBUG -fomit-frame-pointer -ffreestanding -Iinclude 
 

app/jni/ape/filter.c
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
 #include "filter.h"
 #include "demac_config.h"
 
-#ifdef ANDROID 
+#ifdef CPU_ARM
 #include "vector_math16_armv7.h"
 //#include "vector_math16_armv6.h"
 //#include "vector_math16_armv5te.h"

app/jni/flac/Android.mk
@@ -4,8 +4,14 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := flac
 
-LOCAL_SRC_FILES += decoder.c shndec.c arm.S main.c
-LOCAL_CFLAGS += -O3 -Wall -DBUILD_STANDALONE -DCPU_ARM -finline-functions -fPIC -march=armv7-a -mthumb -I. -Iinclude
+LOCAL_SRC_FILES += decoder.c shndec.c main.c
+LOCAL_CFLAGS += -O3 -Wall -DBUILD_STANDALONE -finline-functions -fPIC -I. -Iinclude
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+LOCAL_CFLAGS += -DCPU_ARM
+LOCAL_SRC_FILES += arm.S
+endif
+
 #-DDBG_TIME
 #LOCAL_ARM_MODE := arm
 

app/jni/linux_main.c
@@ -23,7 +23,7 @@
 #define __USE_BSD  1
 #include <getopt.h>
 
-#ifdef __ANDROID__
+#ifdef ANDLINUX
 extern int security_getenforce(void);
 extern int security_setenforce(int value);
 #endif
@@ -47,7 +47,20 @@ void bye(int sig)
 
 static int usage(char *prog) 
 {
-   return printf("Usage: %s [-c card] [-d device] [-s min:sec | -t track_no] [-q] <file>\n", prog);
+   printf("Usage: %s [-x file] [-c card] [-d device] [-s min:sec | -t track_no] [-p num:sz] [-q] (<-i> | <file>)\n", prog);
+   return printf(
+#ifdef ANDLINUX
+		 "-x\tspecify custom xml config (default is /sdcard/.alsaplayer/cards.xml)\n"
+#else
+		 "-x\tspecify custom xml config (default is $HOME/.alsaplayer/cards.xml)\n"
+#endif
+		 "-c/-d\tcard/device numbers, see output of 'cat /proc/asound/pcm' (defaults are 0/0)\n"
+		 "-s\tspecify start point of playback in a file\n"
+		 "-t\tspecify cue file track (audio file defined in cue must be in the same dir)\n"
+		 "-p\tforce number and size of periods (in frames) or fragments (in bytes)\n"
+		 "-q\tquiet mode, suppress extra info\n"
+		 "-i\ttest the selected device and show its information\n"
+	);
 }
 
 void *thd(void *a) 
@@ -68,9 +81,19 @@ void pause_resume(int sig) {
 
 static int parse_cue(struct call_args *args);
 
+static int test_device(int card, int device)
+{
+    ctx = (playback_ctx *) audio_init(0, 0, 0, card, device);
+    if(!ctx) return 1;
+    printf("%s\n", alsa_current_device_info(ctx));	    	
+    audio_exit(0, 0, ctx);
+    return 0; 		
+}
+
+
 int main(int argc, char **argv)
 {
-    int card = 0, device = 0, opt;
+    int card = 0, device = 0, info = 0, opt;
     char *c;
     pthread_t thread;
     sigset_t set;
@@ -85,7 +108,7 @@ int main(int argc, char **argv)
 	signal(SIGUSR1, pause_resume);	
 	signal(SIGUSR2, pause_resume);	
 
-	while ((opt = getopt(argc, argv, "c:d:s:t:q")) != -1) {
+	while ((opt = getopt(argc, argv, "c:d:s:t:qix:p:")) != -1) {
 	    switch (opt) {
 		case 'c':
 		    card = atoi(optarg);
@@ -99,16 +122,43 @@ int main(int argc, char **argv)
 		    args->min = atoi(optarg);
 		    args->sec = atoi(c+1);
 		    break;
+		case 'p':
+		    c = strchr(optarg, ':');
+		    if(!c) return printf("bad argument to -p option\n");
+		    forced_chunks = atoi(optarg);
+		    forced_chunk_size = atoi(c+1);
+		    if(!forced_chunks || !forced_chunk_size)
+			return printf("invalid number/size of periods/fragments %d/%d\n", 
+				forced_chunks, forced_chunk_size);	
+		    break;
 		case 't':
 		    args->track = atoi(optarg);
 		    break; 
 		case 'q':
 		    quiet_run = 1;
-		    break; 	
+		    break;
+		case 'i':
+		    info = 1;
+		    break;
+		case 'x':
+		    ext_cards_file = optarg;
+		    break;			
 		default: /* '?' */
 		    return usage(argv[0]);
 	    }
 	}
+#ifdef ANDLINUX
+	if(security_getenforce() == 1) {
+	    if(!quiet_run) printf("switching to permissive mode: ");	
+	    security_setenforce(0);
+	    if(!quiet_run) printf(security_getenforce() == 0 ? "okay\n" : "failed\n");	
+	}
+#endif
+	if(info) {
+	    quiet_run = 1; 
+	    return test_device(card, device);
+	}
+
 	if(optind >= argc) return usage(argv[0]);
 
 	args->file = strdup(argv[optind]);
@@ -137,12 +187,6 @@ int main(int argc, char **argv)
 	}
 
 	if(args->ftype == -1) return printf("file extension must be .flac, .ape or .mp3\n");
-#ifdef __ANDROID__
-	if(security_getenforce() == 1) {
-	    printf("trying to switch into permissive mode\n");	
-	    security_setenforce(0);
-	}
-#endif
 	ctx = (playback_ctx *) audio_init(0, 0, 0, card, device);
 	if(!ctx) return -1;	
 

app/jni/main.c
@@ -121,7 +121,7 @@ static void *audio_write_thread(void *a)
 #ifdef TEST_TIMING
 		    gettimeofday(&tstart,0);
 #endif
-		    i = alsa_write(ctx, k/f2b);
+		    i = alsa_write(ctx, 0, k/f2b);
 #ifdef TEST_TIMING
 		    gettimeofday(&tstop,0);
 		    timersub(&tstop, &tstart, &tdiff);
@@ -457,6 +457,7 @@ jboolean audio_exit(JNIEnv *env, jobject obj, playback_ctx *ctx)
     }	
     log_info("audio_exit: ctx=%p",ctx);
     audio_stop(ctx, 1);
+    alsa_stop(ctx); /* just to set stop mixer controls in case it wasn't playing  */	
     alsa_exit(ctx);
     alsa_free_mixer_controls(ctx);
     if(ctx->xml_mixp) xml_mixp_close(ctx->xml_mixp);

app/jni/main.h
@@ -16,8 +16,8 @@ extern "C" {
 #endif
 #else
 extern int quiet_run;
-#define log_info(fmt, args...) do { if(!quiet_run) printf("liblossless: [%s] " fmt "\n", __func__, ##args); } while(0)
-#define log_err(fmt, args...)  do { fprintf(stderr, "liblossless: [%s] " fmt "\n", __func__, ##args); } while(0)
+#define log_info(fmt, args...) do { if(!quiet_run) printf("[%s] " fmt "\n", __func__, ##args); } while(0)
+#define log_err(fmt, args...)  do { fprintf(stderr, "[%s] " fmt "\n", __func__, ##args); } while(0)
 #endif
 
 #ifndef timeradd
@@ -99,7 +99,7 @@ extern jboolean audio_resume(JNIEnv *env, jobject obj, playback_ctx *ctx);
 extern int alsa_select_device(playback_ctx *ctx, int card, int device);
 extern int alsa_start(playback_ctx *ctx);
 extern void alsa_stop(playback_ctx *ctx);
-extern ssize_t alsa_write(playback_ctx *ctx, size_t count);
+extern ssize_t alsa_write(playback_ctx *ctx, void *buf, size_t count);
 extern bool alsa_pause(playback_ctx *ctx);
 extern bool alsa_resume(playback_ctx *ctx);
 extern bool alsa_set_default_volume(playback_ctx *ctx);
@@ -115,7 +115,11 @@ extern int alsa_is_offload(playback_ctx *ctx);
 extern int alsa_get_devices(char ***dev_names);
 extern int alsa_is_usb_card(JNIEnv *env, jobject obj, int card);
 extern int alsa_is_offload_device(JNIEnv *env, jobject obj, int card, int device);
+#endif
 extern char *alsa_current_device_info(playback_ctx *ctx);
+#ifndef ANDROID
+extern char *ext_cards_file;
+extern int forced_chunks, forced_chunk_size;
 #endif
 
 /* alsa_offload.c */

app/jni/tinyxml/xmlparser.cpp
@@ -262,15 +262,18 @@ extern "C" struct nvset *xml_dev_find_ctls(void *xml, const char *name, const ch
     DeviceXML *m = (DeviceXML *) xml; 	
     if(!m || !m->is_valid() || m->is_card_only()) return 0;	
     XMLElement *e, *e1, *e2;
+
+
     for(e = m->get_dev_root()->FirstChildElement(); e; e = e->NextSiblingElement()) {
 	if(strcmp(e->Name(), "path") == 0 && e->Attribute("name", name)
 	   && (!value || e->Attribute("value", value))) return m->get_controls(e);
+	
 	const char *c = e->Attribute("name");
 	for(e1 = m->get_card_root()->FirstChildElement(); e1; e1 = e1->NextSiblingElement()) {
 	    if(strcmp(e1->Name(), "path") == 0 && e1->Attribute("name", c)) {
 		for(e2 = e1->FirstChildElement(); e2; e2 = e2->NextSiblingElement()) {
 		    if(strcmp(e2->Name(), "path") == 0 && e2->Attribute("name", name)
-		       && (!value || e2->Attribute("value", value))) return m->get_controls(e2);
+		       && (!value || e2->Attribute("value", value) )) return m->get_controls(e2);
 		}
 	    }	
 	}    	

app/jni/wav_main.c
@@ -152,7 +152,6 @@ int wav_play(JNIEnv *env, jobject obj, playback_ctx *ctx, jstring jfile, int sta
     int i, k, read_bytes, ret = 0, fd = -1;
     int samplerate = 0, channels = 0, bps = 0, b2f;		/* b2f = bytes->frames */
     void *mptr, *mend, *mm = MAP_FAILED;
-    int8_t *src, *dst; 
     void *pcmbuf = 0; 
     const char *file = 0;
     off_t flen = 0; 
@@ -316,35 +315,24 @@ int wav_play(JNIEnv *env, jobject obj, playback_ctx *ctx, jstring jfile, int sta
 		i = (mend - mptr < read_bytes) ? mend - mptr : read_bytes;
 		log_info("remapped");
 	    }
-	    
-	    switch(format->fmt) {
-		case SNDRV_PCM_FORMAT_S16_LE:		
-		case SNDRV_PCM_FORMAT_S24_3LE:
-		case SNDRV_PCM_FORMAT_S32_LE:
-		    memcpy(pcmbuf, mptr, i);
-		    break;	
-		case SNDRV_PCM_FORMAT_S24_LE:
-		    src = (int8_t *) mptr;
-		    dst = (int8_t *) pcmbuf;
-		    for(k = 0; k < i/3; k++) {
-			dst[0] = src[0];	
-			dst[1] = src[1];	
-			dst[2] = src[2];	
-			dst[3] = (src[2] < 0) ? 0xff : 0;
-			src += 3; dst += 4;
-		    }
-		    break;
-		default:
-		    log_err("internal error: format not supported");
-		    ret = LIBLOSSLESS_ERR_INIT;
-		    goto done; 	
+
+	    if(format->fmt == SNDRV_PCM_FORMAT_S24_LE) {
+		int8_t *src = (int8_t *) mptr;
+		int8_t *dst = (int8_t *) pcmbuf;
+		for(k = 0; k < i/3; k++) {
+		    dst[0] = src[0];	
+		    dst[1] = src[1];	
+		    dst[2] = src[2];	
+		    dst[3] = (src[2] < 0) ? 0xff : 0;
+		    src += 3; dst += 4;
+		}
 	    }
-	    mptr += i;	
 
 	    pthread_mutex_lock(&ctx->mutex);
             switch(ctx->state) {
-                case STATE_PLAYING:
-                    i = alsa_write(ctx, i/b2f);
+                case STATE_PLAYING:		/* for S24_LE, bytes are copied to pcmbuf (= alsa priv->buf) above */
+		    if(format->fmt == SNDRV_PCM_FORMAT_S24_LE) k = alsa_write(ctx, 0, i/b2f);
+		    else k = alsa_write(ctx, mptr, i/b2f); /* otherwise supply direct pointer to mmapped space*/		
                     break;
                 case STATE_PAUSED:
                     pthread_mutex_unlock(&ctx->mutex);
@@ -359,10 +347,11 @@ int wav_play(JNIEnv *env, jobject obj, playback_ctx *ctx, jstring jfile, int sta
 		    goto done;	
             }
 	    pthread_mutex_unlock(&ctx->mutex);
-	    if(i <= 0) {
+	    if(k <= 0) {
 		if(ctx->alsa_error) ret = LIBLOSSLESS_ERR_IO_WRITE;
 		break;
 	    }
+	    mptr += i;	
 	}
 
     done:

app/src/main/res/values-ru/strings.xml
@@ -70,7 +70,7 @@
 <string name="strMplayerError">Ошибка медиа-плеера</string>
 <string name="strDriverMode">Прямой доступ к драйверу</string>
 <string name="strSaveBooks">Автосохранение закладок</string>
-<string name="strAbout">Android плеер alsaPlayer для звуковых файлов без потерь. Дата сборки Apr 17, 2015</string>
+<string name="strAbout">Android плеер alsaPlayer для звуковых файлов без потерь. Дата сборки Apr 18, 2015</string>
 <string name="strAbout1">О программе</string>
 <string name="strQuit">Выход</string>
 <string name="strHideExt">Не показывать расширений файлов</string>

app/src/main/res/values/strings.xml
@@ -70,7 +70,7 @@
 <string name="strMplayerError">Media player error </string>
 <string name="strDriverMode">Direct hardware access</string>
 <string name="strSaveBooks">Auto-save bookmarks</string>
-<string name="strAbout">Android player for lossless audio files built on Apr 17, 2015</string>
+<string name="strAbout">Android player for lossless audio files built on Apr 18, 2015</string>
 <string name="strAbout1">About</string>
 <string name="strQuit">Quit</string>
 <string name="strNightMode">Night mode</string>